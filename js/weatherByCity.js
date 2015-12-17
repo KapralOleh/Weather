928f25dd320d452e03a88896e459cf2027ae3e88
Oleh <kapraloleh@gmail.com>
2015-12-17 09:09:17 +0200

    Update weatherByCity.js

--- js/weatherByCity.js
+++ js/weatherByCity.js
@@ -1,7 +1,7 @@
 function getWeatherByCity(lang, fnOK, fnError, cityName) {
     $.getJSON(
         'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
-        + cityName + '&cnt=8&units=metric' + '&lang=' + lang + '&callback=?',
+        + cityName + '&APPID=c5921925e0f07a88ab8d9003fa7401d8&cnt=8&units=metric' + '&lang=' + lang + '&callback=?',
         function (data) {
             fnOK.call(this, data);
         }
